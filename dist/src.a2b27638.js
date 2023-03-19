39d1063d942c556b3fb49603bd6d9f771373e858
GatoRoj <escobar.avila.marcos@hotmail.com>
2023-03-19 14:19:13 -0300

    mobile2

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -32679,7 +32679,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63918" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64936" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
